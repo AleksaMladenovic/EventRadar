5793072ddecbf8758ab1eff55eff8a92ee879262
Aleksa <aleksa.mladenoviccc03@gmail.com>
2025-10-03 19:58:35 +0200

    style(theme): Add app theme and custom colors

--- EventRadarApp/app/src/main/java/com/eventradar/navigation/Screens.kt
+++
@@ -1,7 +0,0 @@
-package com.eventradar.navigation
-
-sealed class Screen(val route: String){
-    object LoginScreen: Screen("login_screen")
-    object MapScreen: Screen("map_screen")
-    object RegisterScreen: Screen("register_screen")
-}
--- EventRadarApp/app/src/main/java/com/eventradar/ui/theme/Color.kt
+++ EventRadarApp/app/src/main/java/com/eventradar/ui/theme/Color.kt
@@ -2,10 +2,30 @@ package com.eventradar.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val md_theme_light_primary = Color(0xFF0061A4)
+val md_theme_light_onPrimary = Color(0xFFFFFFFF)
+val md_theme_light_secondary = Color(0xFF535F70)
+val md_theme_light_onSecondary = Color(0xFFFFFFFF)
+val md_theme_light_error = Color(0xFFBA1A1A)
+val md_theme_light_onError = Color(0xFFFFFFFF)
+val md_theme_light_background = Color(0xFFFDFCFF)
+val md_theme_light_onBackground = Color(0xFF1A1C1E)
+val md_theme_light_surface = Color(0xFFFDFCFF)
+val md_theme_light_onSurface = Color(0xFF1A1C1E)
+val md_theme_light_surfaceVariant = Color(0xFFDFE2EB)
+val md_theme_light_onSurfaceVariant = Color(0xFF43474E)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+
+// Dark Theme Colors
+val md_theme_dark_primary = Color(0xFF9FCAFF)
+val md_theme_dark_onPrimary = Color(0xFF003258)
+val md_theme_dark_secondary = Color(0xFFBBC7DB)
+val md_theme_dark_onSecondary = Color(0xFF253140)
+val md_theme_dark_error = Color(0xFFFFB4AB)
+val md_theme_dark_onError = Color(0xFF690005)
+val md_theme_dark_background = Color(0xFF1A1C1E)
+val md_theme_dark_onBackground = Color(0xFFE2E2E6)
+val md_theme_dark_surface = Color(0xFF1A1C1E)
+val md_theme_dark_onSurface = Color(0xFFE2E2E6)
+val md_theme_dark_surfaceVariant = Color(0xFF43474E)
+val md_theme_dark_onSurfaceVariant = Color(0xFFC3C7CF)
